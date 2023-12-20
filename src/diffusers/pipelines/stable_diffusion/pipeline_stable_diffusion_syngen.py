0a24ba9dd451d1f500a2043e8e1f93f69e35e3b7
Eran Hirsch <hirscheran@gmail.com>
2023-12-20 18:04:28 +0200

    Fix import and un-necessary list comprehension

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_syngen.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion_syngen.py
@@ -1,4 +1,17 @@
-import itertools
+# Copyright 2023 The SynGen Authors and The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from typing import Any, Callable, Dict, Optional, Union, List, Tuple
 
 import torch
@@ -500,7 +513,7 @@ class StableDiffusionSynGenPipeline(StableDiffusionPipeline):
                         )
                         # check if all wp_indices are not already in collected_spacy_indices
                         if wp_indices and (wp_indices not in curr_collected_wp_indices) and all(
-                                [wp_idx not in collected_spacy_indices for wp_idx in wp_indices]):
+                                wp_idx not in collected_spacy_indices for wp_idx in wp_indices):
                             curr_collected_wp_indices.append(wp_indices)
                             break
 
